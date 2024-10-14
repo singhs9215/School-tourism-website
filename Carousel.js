f6834bb2c6be200f7c27203d63431f85d87e7e68
singhs9215 <58443210+singhs9215@users.noreply.github.com>
2024-10-14 18:29:15 +1300

    Attraction item pages

--- Carousel.js
+++ Carousel.js
@@ -35,15 +35,36 @@ $('.Carousel-Main').slick({
   $('.Carousel-Attraction').slick({
     centerMode: true,
     centerPadding: '60px',
-    slidesToShow: 3,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
       {
         breakpoint: 768,
         settings: {
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 2
         }
       },
       {
