418e575c2f1e31b4639a1fe168b0b53ba68e66f9
singhs9215 <Singhs9215@outlook.com>
2024-10-21 08:54:49 +1300

    Navbar items complete

--- Carousel.js
+++ Carousel.js
@@ -1,35 +1,10 @@
 $(document).ready(function(){
 $('.Carousel-Main').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    slidesToShow: 1,
+    slidesToScroll: 1,
   });
 
   $('.Carousel-Attraction').slick({
